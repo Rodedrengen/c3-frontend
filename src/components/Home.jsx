f3e00d12b976f2f7fa70c284a9b0fe6bb1295f5d
Frederik <gammarik@gmail.com>
2020-11-10 16:57:31 +0100

    adds welcome page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,17 @@ import React from "react";
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <h1>Welcome</h1>
+            <h2>How to get started with this project:</h2>
+            <ol>
+                <li>Run 'npm install' to install dependencies</li>
+                <li>Run 'npm start' to begin local server</li>
+                <li>To add a new page, add it to the Switch in App.js, and the header.jsx component
+                    <ul>
+                        <li>Make sure the 'to' attribute in Header matches the 'path' attribute in App</li>
+                    </ul>
+                </li>
+            </ol>
         </>
     );
 }
